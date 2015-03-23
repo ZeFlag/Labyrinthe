bee3ee9a96dc05671e0389d3e2ebca59c641e054
alex-godro <alex_godro@hotmail.com>
2015-03-22 20:28:34 -0400

    Issue

--- zeMaze/AffichageFond.cpp
+++ zeMaze/AffichageFond.cpp
@@ -1,59 +1,147 @@
 #include "AffichageFond.h"
 
 
-void Afficher(Ligne Grille[],int fond, int mur, int sou, int mario, Position Mario)
+void Afficher(Ligne Grille[], int fond, int mur, int sou, int mario, Position Mario)
 {
 	AfficherImage(fond, 0, 0);
 	AfficherObject(Grille, NbMur, mur);
 	AfficherObject(Grille, NbSou, sou);
 	AfficherImage(mario, Mario.x*NbPixelsParCase, Mario.y*NbPixelsParCase);  //Affichage de mario
-	AfficherImage(koopa, Koopa.x*NbPixelsParCase, Koopa.y*NbPixelsParCase);  //Affichage de koopa
-	AfficherImage(goomba, Goomba.x*NbPixelsParCase, Goomba.y*NbPixelsParCase);  //Affichage de Goomba
-	AfficherImage(bomb, Bomb.x*NbPixelsParCase, Bomb.y*NbPixelsParCase);  //Affichage de Bomb
 	RafraichirFenetre();
 }
 
 
 void InitialiserGrille(Ligne Grille[])
 {
-   for(int i=0;i<NbCases;i++)
-   {                                                     //Initialise la grille de jeu à vide.
-      for(int j=0;j<NbCases;j++)
-      {
-         Grille[i].Colonne[j]=Vide;
-      }
-   }
+	for (int i = 0; i<NbCases; i++)
+	{                                                     //Initialise la grille de jeu à vide.
+		for (int j = 0; j<NbCases; j++)
+		{
+			Grille[i].Colonne[j] = Vide;
+		}
+	}
 }
 
-void PlacerObject(Ligne Grille[],int NbObject, int ImageId)
+void GenererMaze(Ligne Grille[], int ImageId)
 {
+	srand(time(0));
 
-   for(int Cpt=0;Cpt<NbObject;Cpt++)
-   {
-      int i=rand()%NbCases;
-      int j=rand()%NbCases;                               //Définition de la fonction PlacerObject
+	const int maze_size_x = 18;
+	const int maze_size_y = 18;
+	vector < vector < bool > > maze;
+	list < pair < int, int> > drillers;
 
-      if(Grille[i].Colonne[j]==Vide)
-      {
-         Grille[i].Colonne[j]=ImageId;
-      }
-      else
-      {
-         Cpt--;
-      }
-   }
+	maze.resize(maze_size_y);
+	for (size_t y = 0; y<maze_size_y; y++)
+		maze[y].resize(maze_size_x);
+
+	for (size_t x = 0; x<maze_size_x; x++)
+		for (size_t y = 0; y<maze_size_y; y++)
+			maze[y][x] = false;
+
+	drillers.push_back(make_pair(maze_size_x / 2, maze_size_y / 2));
+	while (drillers.size()>0)
+	{
+		list < pair < int, int> >::iterator m, _m, temp;
+		m = drillers.begin();
+		_m = drillers.end();
+		while (m != _m)
+		{
+			bool remove_driller = false;
+			switch (rand() % 4)
+			{
+			case 0:
+				(*m).second -= 2;
+				if ((*m).second<0 || maze[(*m).second][(*m).first])
+				{
+					remove_driller = true;
+					break;
+				}
+				maze[(*m).second + 1][(*m).first] = true;
+				break;
+			case 1:
+				(*m).second += 2;
+				if ((*m).second >= maze_size_y || maze[(*m).second][(*m).first])
+				{
+					remove_driller = true;
+					break;
+				}
+				maze[(*m).second - 1][(*m).first] = true;
+				break;
+			case 2:
+				(*m).first -= 2;
+				if ((*m).first<0 || maze[(*m).second][(*m).first])
+				{
+					remove_driller = true;
+					break;
+				}
+				maze[(*m).second][(*m).first + 1] = true;
+				break;
+			case 3:
+				(*m).first += 2;
+				if ((*m).first >= maze_size_x || maze[(*m).second][(*m).first])
+				{
+					remove_driller = true;
+					break;
+				}
+				maze[(*m).second][(*m).first - 1] = true;
+				break;
+			}
+			if (remove_driller)
+				m = drillers.erase(m);
+			else
+			{
+				drillers.push_back(make_pair((*m).first, (*m).second));
+				// uncomment the line below to make the maze easier 
+				//if (rand()%2) 
+				drillers.push_back(make_pair((*m).first, (*m).second));
+
+				maze[(*m).second][(*m).first] = true;
+				++m;
+			}
+		}
+	}
+
+	// Done 
+	for (size_t y = 0; y<maze_size_y; y++)
+		for (size_t x = 0; x<maze_size_x; x++)
+		{
+		if (maze[y][x] == false)
+			Grille[y].Colonne[x] = ImageId;
+		}
+
 }
 
-void AfficherObject(Ligne Grille[],int NbObject, int ImageId)
+void PlacerObject(Ligne Grille[], int NbObject, int ImageId)
 {
-   for(int i=0;i<NbCases;i++)
-   {
-      for(int j=0;j<NbCases;j++)
-      {
-         if(Grille[i].Colonne[j]==ImageId)                                       //Définition de la fonction AfficherObject
-         {
-            AfficherImage(ImageId, i*NbPixelsParCase,j*NbPixelsParCase);
-         }
-      }
-   }
+	GenererMaze(Grille, ImageId);
+
+	/*for(int Cpt=0;Cpt<NbObject;Cpt++)
+	{
+	int i=rand()%NbCases;
+	int j=rand()%NbCases;
+
+	if(Grille[i].Colonne[j]==Vide)
+	{
+	Grille[i].Colonne[j]=ImageId;
+	}
+	else
+	{
+	Cpt--;
+	}
+	}*/
+}
+
+void AfficherObject(Ligne Grille[], int NbObject, int ImageId)
+{
+	for (int i = 0; i<NbCases; i++)
+	{
+		for (int j = 0; j<NbCases; j++)
+		{
+			if (Grille[i].Colonne[j] == ImageId)
+			{
+				AfficherImage(ImageId, i*NbPixelsParCase, j*NbPixelsParCase);
+			}
+		}
+	}
 }
--- zeMaze/AffichageFond.h
+++ zeMaze/AffichageFond.h
@@ -1,28 +1,31 @@
 #ifndef _AffichageFond_h
 #define _AffichageFond_h
+
+#include <time.h>
+#include <vector>
+#include <list>
 #include "Constante.h"
 #include "sdlclg.h"
 
+//Déclaration des structures
 struct Position
 {
 	int x;
-	int y;                                //Déclaration des structures
+	int y;
 };
 
-struct Ligne 
-{ 	
-	ImageId Colonne[NbCases]; 
-};  
+struct Ligne
+{
+	ImageId Colonne[NbCases];
+};
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Nom: Afficher
 //But: Affichage du fond, des murs, des sous, des ennemis et de Mario.
 //Intrants: La grille de jeu, l'ImageId du fond, des murs, des sous, des ennemis et de Mario, ainsi que leur position.
 //Extrants: Aucun
-void Afficher(Ligne Grille[],int fond, int mur, int sou, int mario, Position Mario);
+void Afficher(Ligne Grille[], int fond, int mur, int sou, int mario, Position Mario);
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Nom: InitialiserGrille
 //But: Initialise la grille de jeu en mettant les cases à vide.
@@ -31,20 +34,24 @@ void Afficher(Ligne Grille[],int fond, int mur, int sou, int mario, Position Mar
 void InitialiserGrille(Ligne Grille[]);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Nom: GenererMaze
+//But: Génère un labyrinthe aléatoire
+//Intrants: La grille de jeu, et l'imageId de l'image
+//Extrants: Aucun
+void GenererMaze(Ligne Grille[], int ImageId);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Nom: PlacerObject
 //But: Placer les objects (comme les murs et les sous)
 //Intrants: La grille de jeu, le nombre d'objects a initialiser et l'imageId de l'image
 //Extrants: Aucun
-void PlacerObject(Ligne Grille[],int NbObject, int ImageId);
+void PlacerObject(Ligne Grille[], int NbObject, int ImageId);
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Nom: AfficherObject
 //But: Afficher les objects a leur position respective
 //Intrants: La grille de jeu, le nombre d'objects a initialiser et l'imageId de l'image a afficher.
 //Extrants: Aucun
-void AfficherObject(Ligne Grille[],int NbObject,int ImageId);
-
+void AfficherObject(Ligne Grille[], int NbObject, int ImageId);
 
 #endif _AffichageFond_h
